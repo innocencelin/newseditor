f6acdb6f228b63ad06e5e5de9fe544bc850ac883
Innocence Lin <innocence.lin@gmail.com>
2012-12-26 21:28:16 +0800

    constanttitleoccurrence, constanttitlecacheday

--- src/globalconfig.py
+++ src/globalconfig.py
@@ -13,6 +13,12 @@ cmapi.registerModel(PageConstantTitle)
 def getTitleSeparators():
     return cmapi.getItemValue('titleseparator', '_|-')
 
+def getConstantTitleOccurrence():
+    return cmapi.getItemValue('constanttitleoccurrence', 1)
+
+def getConstantTitleCacheDay():
+    return cmapi.getItemValue('constanttitlecacheday', 7)
+
 def isConstantTitle(url, title):
     if not url:
         return False
@@ -23,14 +29,14 @@ def isConstantTitle(url, title):
     if not record:
         record = {}
     count = record.get('c', 0)
-    isconstant = count >= 3
+    isconstant = count >= getConstantTitleOccurrence()
 
     nnow = datetime.datetime.utcnow()
     record['c'] = count + 1
     record['u'] = nnow
     if len(value) > 20:
         for ik, iv in value.items():
-            if (nnow - iv['u']).days >= 7:
+            if (nnow - iv['u']).days >= getConstantTitleCacheDay():
                 del value[ik]
     value[title] = record
     success = cmapi.saveItem(key, value, modelname=PageConstantTitle)
